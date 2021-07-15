ead5cc320febd074a592cc02b072874d2613932c
Amos German <germanamos.ag@gmail.com>
2021-07-15 16:57:39 +0200

    initial commit

---
+++ resources/views/students/create.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row" style="margin-bottom: 20px;">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h3>Add Student</h3>
+            </div>
+        </div>
+    </div>
+
+    @if ($errors->any())
+        <div class -="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form action="{{ route('students.store') }}" method="POST">
+        @csrf
+
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>First Name:</strong>
+                    <input type="text" name="firstname" class="form-control">
+                    <strong>Last Name:</strong>
+                    <input type="text" name="lastname" class="form-control">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Course:</strong>
+                    <input type="text" name="course" class="form-control">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Course Fee:</strong>
+                    <input type="number" name="fee" class="form-control">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-success">Add Student</button>
+            </div>
+        </div>
+
+    </form>
+@endsection
---
+++ resources/views/students/view.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row" style="margin-bottom: 20px;">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h4>Student Details</h4>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Name:</strong>
+                {{ $student->firstname }}
+                {{ $student->lastname }}
+               
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Course:</strong>
+                {{ $student->course }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Fee:</strong>
+                {{ $student->fee }}
+            </div>
+        </div>
+    </div>
+@endsection
